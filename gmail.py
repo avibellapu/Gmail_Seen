3707b855181fbd35a0a97450a449661fb938f57c
root <root@ip-172-31-10-56.ca-central-1.compute.internal>
2018-06-07 20:53:36 +0000

    Cleans up extra code

--- gmail.py
+++ gmail.py
@@ -4,7 +4,7 @@ import getpass
 obj = imaplib.IMAP4_SSL('imap.gmail.com', '993')
 name = raw_input("Username:")
 paswd = getpass.getpass("Password:")
-obj.login(name,paswd) #Enter your Username and Password
-obj.select('Inbox')  # <--- it will select inbox
+obj.login(name,paswd) 
+obj.select('Inbox') 
 typ ,data = obj.search(None,'UnSeen')
 obj.store(data[0].replace(' ',','),'+FLAGS','\Seen')
